fc1f9ad07426d73e4e2a368848a315ec6eb053c9
josepro66 <andrewscreative13@gmail.com>
2026-07-16 16:52:49 -0500

    Updates

--- assets/ConfiguratorWrapper-BhKDxgod.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./three-BIB78AvD.js";import{r as o}from"./react-DYT_lSRc.js";const e=({children:r})=>t.jsx(o.Suspense,{fallback:null,children:r});export{e as default};
---
+++ assets/ConfiguratorWrapper-Cw5vb8km.js
@@ -0,0 +1,1 @@
+import{j as t}from"./three-CyBL5o2h.js";import{r as o}from"./react-DYT_lSRc.js";const e=({children:r})=>t.jsx(o.Suspense,{fallback:null,children:r});export{e as default};
